a7fc42536c90eadcbcf639cd183f53ad8ad73f07
yogesh niranjan <yogeshniranjan786@gmail.com>
2023-07-26 11:38:46 +0530

    Part1 - Home Page Template Added

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import Navbar from "../features/navbar/Navbar";
+import ProductList from "../features/product-list/ProductList";
+
+function Home() {
+  return (
+    <Navbar>
+      <ProductList></ProductList>
+    </Navbar>
+  );
+}
+
+export default Home;
